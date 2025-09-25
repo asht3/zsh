fc4b73e29907ab1942f83125a7a89109980e1029
tay_a <ashleytay448@gmail.com>
2025-09-24 21:15:18 -0700

    Added a title adn colors to differentiate shell from user input

--- include/utils.h
+++ include/utils.h
@@ -3,11 +3,13 @@
 
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 // Global variable
 extern char* saved_ptr;
 
 void write_error(const char* msg);
+void print_title();
 int my_strlen(const char* str_1);
 char* my_strchr(const char* str, int search_char);
 char* my_strtok(char* str, const char* delim);
--- src/main.c
+++ src/main.c
@@ -8,12 +8,15 @@ int main (int argc, char** argv, char** envp) {
         return EXIT_FAILURE;
     }
 
+    print_title();
+
     char* input = NULL;
     size_t input_len = 0;
     ssize_t read_size;
     while (1) {
         if (isatty(STDIN_FILENO)) {
-            write(STDOUT_FILENO, "my_zsh $> ", 10);
+            // write(STDOUT_FILENO, "my_zsh $> ", 10);
+            printf("\033[1;36mmy_zsh $> \033[0m");
         }
         read_size = getline(&input, &input_len, stdin);
 
